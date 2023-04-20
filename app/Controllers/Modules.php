c482895f2f7dbee82fa78c28ec24a838ec188412
ianfajar10 <ian.fajar10@gmail.com>
2023-04-20 15:18:55 +0700

    S UPDATE : (Membedakan akses di sidebar - 20042023)

--- app/Controllers/Modules.php
+++ app/Controllers/Modules.php
@@ -8,14 +8,25 @@ class Modules extends BaseController
 {
     public function index()
     {
-        $data = [
-            'sidebars' => [
-                'dashboard' => ['Beranda', 'bi bi-grid'],
-                'menu-list' => ['Daftar Menu', 'bi bi-book"'],
-                'upload' => ['Unggah', 'bi bi-pencil-square'],
-                'profile' => ['Profil', 'bi bi-person']
-            ]
-        ];
+        $check_role = session()->get('role');
+        if ($check_role == 1) {
+            $data = [
+                'sidebars' => [
+                    'dashboard' => ['Beranda', 'bi bi-grid'],
+                    // 'menu-list' => ['Daftar Menu', 'bi bi-book"'],
+                    'upload' => ['Unggah', 'bi bi-pencil-square'],
+                    'profile' => ['Profil', 'bi bi-person']
+                ]
+            ];
+        } else {
+            $data = [
+                'sidebars' => [
+                    'dashboard' => ['Beranda', 'bi bi-grid'],
+                    'menu-list' => ['Daftar Menu', 'bi bi-book"'],
+                    'profile' => ['Profil', 'bi bi-person']
+                ]
+            ];
+        }
         return ($data);
     }
 
