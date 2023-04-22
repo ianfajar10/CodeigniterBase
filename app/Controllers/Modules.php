a4c6803241017b7efa7058c128a1eba0282f3c3b
ianfajar10 <ian.fajar10@gmail.com>
2023-04-23 00:01:53 +0700

    S UPDATE : (Merubah kondisi sidebar agar efisien - 23042023)

--- app/Controllers/Modules.php
+++ app/Controllers/Modules.php
@@ -9,38 +9,26 @@ class Modules extends BaseController
     public function index()
     {
         $check_role = session()->get('role');
+        $data['all'] = [
+            'dashboard' => ['Beranda', 'bi bi-grid'],
+            'menulist' => ['Daftar Menu', 'bi bi-book"'],
+            'menulist/detail' => ['Detail Menu', '-'],
+            'upload' => ['Unggah', 'bi bi-pencil-square'],
+            'profile' => ['Profil', 'bi bi-person']
+        ];
         if ($check_role == 1) {
-            $data = [
-                'all' => [
-                    'dashboard' => ['Beranda', 'bi bi-grid'],
-                    'menulist' => ['Daftar Menu', 'bi bi-book"'],
-                    'menulist/detail' => ['Detail Menu', '-'],
-                    'upload' => ['Unggah', 'bi bi-pencil-square'],
-                    'profile' => ['Profil', 'bi bi-person']
-                ],
-                'sidebars' => [
-                    'dashboard' => ['Beranda', 'bi bi-grid'],
-                    'upload' => ['Unggah', 'bi bi-pencil-square'],
-                    'profile' => ['Profil', 'bi bi-person']
-                ],
+            $data['sidebars'] = [
+                'dashboard' => ['Beranda', 'bi bi-grid'],
+                'upload' => ['Unggah', 'bi bi-pencil-square'],
+                'profile' => ['Profil', 'bi bi-person']
             ];
         } else {
-            $data = [
-                'all' => [
-                    'dashboard' => ['Beranda', 'bi bi-grid'],
-                    'menulist' => ['Daftar Menu', 'bi bi-book"'],
-                    'menulist/detail' => ['Detail Menu', '-'],
-                    'upload' => ['Unggah', 'bi bi-pencil-square'],
-                    'profile' => ['Profil', 'bi bi-person']
-                ],
-                'sidebars' => [
-                    'dashboard' => ['Beranda', 'bi bi-grid'],
-                    'menulist' => ['Daftar Menu', 'bi bi-book"'],
-                    'profile' => ['Profil', 'bi bi-person']
-                ]
+            $data['sidebars'] = [
+                'dashboard' => ['Beranda', 'bi bi-grid'],
+                'menulist' => ['Daftar Menu', 'bi bi-book"'],
+                'profile' => ['Profil', 'bi bi-person']
             ];
         }
         return ($data);
     }
-
 }
