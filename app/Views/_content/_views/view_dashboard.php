5126124fd45b1391e8efdc0516752b88235d052e
ianfajar10 <ian.fajar10@gmail.com>
2023-06-12 11:24:47 +0700

    Update : (Carrousel - #12062023)

--- app/Views/_content/_views/view_dashboard.php
+++ app/Views/_content/_views/view_dashboard.php
@@ -22,21 +22,29 @@
             </div>
         </div>
     </div>
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel" style="height:fit-content; background-color:black;">
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="height:fit-content; background-color:black;">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="<?= base_url().'assets/img/slides-1.jpeg'?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
+                <img src="<?= base_url() . 'assets/img/slides-1.jpeg' ?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
             </div>
             <div class="carousel-item">
-                <img src="<?= base_url().'assets/img/slides-2.jpeg'?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
+                <img src="<?= base_url() . 'assets/img/slides-2.jpeg' ?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
             </div>
             <div class="carousel-item">
-                <img src="<?= base_url().'assets/img/slides-3.jpeg'?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
+                <img src="<?= base_url() . 'assets/img/slides-3.jpeg' ?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
             </div>
             <div class="carousel-item">
-                <img src="<?= base_url().'assets/img/slides-4.jpeg'?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
+                <img src="<?= base_url() . 'assets/img/slides-4.jpeg' ?>" class="d-block w-100" alt="..." style="height: 350px; object-fit: contain;">
             </div>
         </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
 <script>
