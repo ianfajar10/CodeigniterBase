c2f81652ec84fe330b5988636bf843c69c007dcf
ianfajar10 <ian.fajar10@gmail.com>
2024-01-29 23:02:28 +0700

    Create : Function migrate File Model

---
+++ app/Database/Migrations/2024-01-29-155819_FileTable.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CodeigniterBase\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class FileTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'price' => [
+                'type' => 'DECIMAL',
+                'constraint' => '10,2',
+            ],
+            'description' => [
+                'type' => 'TEXT',
+            ],
+            'file' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'type' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tbl_files');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tbl_files');
+    }
+}
